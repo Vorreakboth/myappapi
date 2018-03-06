c638480873820d5e47c663c8bf03a21f9ff62268
Computer <schoolguideproject@gmail.com>
2018-03-06 10:35:06 +0700

    init

---
+++ iOS_API/CreateViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  iOS_API
+//
+//  Created by Computer on 3/5/18.
+//  Copyright © 2018 Computer. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    let URL_USER_REGISTER = "https://myappapi.000webhostapp.com/v1/register.php"
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldPhone: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    @IBAction func buttonRegister(_ sender: UIButton) {
+        let parameters: Parameters=[
+            "username":textFieldUsername.text!,
+            "password":textFieldPassword.text!,
+            "name":textFieldName.text!,
+            "email":textFieldEmail.text!,
+            "phone":textFieldPhone.text!
+        ]
+        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON
+            {
+                response in
+                //printing response
+                print(response)
+                
+                //getting the json value from the server
+                if let result = response.result.value {
+                    
+                    //converting it as NSDictionary
+                    let jsonData = result as! NSDictionary
+                    
+                    //displaying the message in label
+                    self.labelMessage.text = jsonData.value(forKey: "message") as! String?
+                    self.labelMessage.isHidden = false
+                }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
